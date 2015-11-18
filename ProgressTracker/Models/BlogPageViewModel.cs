01c815e73ae0aab41153de47b916f23caef96793
kjaek <kjaek@outlook.com>
2015-11-18 20:33:20 +1300

    Added blog page controller / model to represent blog pages for the site

---
+++ ProgressTracker/Models/BlogPageViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProgressTracker.Models
+{
+    public class BlogPageViewModel
+    {
+        public string Title { get; set; }
+
+        public string Excerpt { get; set; }
+
+        public string Content { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+
+        public DateTime ModifiedDate { get; set; }
+
+        public BlogPageViewModel(string title, string excerpt, string content)
+        {
+            Title = title;
+            Excerpt = excerpt;
+            Content = content;
+            DateTime currentDate = DateTime.Now.ToUniversalTime();
+            CreatedDate = currentDate;
+            ModifiedDate = currentDate;
+        }
+    }
+}
