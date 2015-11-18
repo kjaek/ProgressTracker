696136a5542a3d3d533caa6aef7dfc0ee1b6eb9f
kjaek <kjaek@outlook.com>
2015-11-18 20:41:29 +1300

    Added in blog page view + a cs file for retrieving blog page data

--- ProgressTracker/Controllers/BlogPageController.cs
+++ ProgressTracker/Controllers/BlogPageController.cs
@@ -3,14 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ProgressTracker.Models;
 
 namespace ProgressTracker.Controllers
 {
     public class BlogPageController : Controller
     {
         // GET: BlogPage
-        public ActionResult Index()
+        public ActionResult Index(string id)
         {
+            //BlogPageViewModel vm = new BlogPageViewModel();
             return View();
         }
     }
---
+++ ProgressTracker/Models/BlogPageDataModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProgressTracker.Models
+{
+    // This class will be used to retrieve Blog Page data from the database
+    public class BlogPageDataModel
+    {
+    }
+}
